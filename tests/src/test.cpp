11ce4cce8d38e736f5ab6e186f5a2c3d9c464f0b
Takayama Fumihiko <tekezo@pqrs.org>
2026-06-23 22:10:27 +0900

    Use adopt_cf_ptr

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -1,7 +1,7 @@
 #include <boost/ut.hpp>
 #include <pqrs/osx/iokit_hid_event_system_client.hpp>
 
-int main(void) {
+int main() {
   using namespace boost::ut;
   using namespace boost::ut::literals;
 
@@ -9,6 +9,20 @@ int main(void) {
     pqrs::osx::iokit_hid_event_system_client client;
 
     expect(client.make_registry_entry_ids().size() > 0);
+  };
+
+  "invalid_registry_entry_id"_test = [] {
+    pqrs::osx::iokit_hid_event_system_client client;
+
+    constexpr pqrs::osx::iokit_registry_entry_id::value_t registry_entry_id(0);
+
+    expect(client.get_initial_key_repeat(registry_entry_id) == std::nullopt);
+    expect(client.get_key_repeat(registry_entry_id) == std::nullopt);
+    expect(client.get_caps_lock_delay_override(registry_entry_id) == std::nullopt);
+
+    client.set_initial_key_repeat(registry_entry_id, 0);
+    client.set_key_repeat(registry_entry_id, 0);
+    client.set_caps_lock_delay_override(registry_entry_id, 0);
   };
 
   return 0;
