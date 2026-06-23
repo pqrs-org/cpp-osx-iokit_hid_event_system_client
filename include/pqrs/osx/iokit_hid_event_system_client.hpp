6fae78040ff6ffaeb0bbf634a517a367193f8e30
Takayama Fumihiko <tekezo@pqrs.org>
2026-06-23 22:11:10 +0900

    v1.2.0

--- include/pqrs/osx/iokit_hid_event_system_client.hpp
+++ include/pqrs/osx/iokit_hid_event_system_client.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// pqrs::osx::iokit_hid_event_system_client v1.1
+// pqrs::osx::iokit_hid_event_system_client v1.2.0
 
 // (C) Copyright Takayama Fumihiko 2020.
 // Distributed under the Boost Software License, Version 1.0.
